186462032781b7df166ba0977bca9145f839a19d
Lukas Kuppers <lkuppers11@gmail.com>
2020-12-11 15:00:39 -0800

    Add basic camera controller

---
+++ Unity-Proj/Assets/Scripts/3D/CameraMovementController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovementController : MonoBehaviour
+{
+    private MouseInput mouseInput;
+
+    private GameObject cameraRoot;
+    private Vector3 rotState;
+
+    void Start()
+    {
+        mouseInput = gameObject.AddComponent<MouseInput>();
+        cameraRoot = gameObject;
+        rotState = new Vector3(0, 0, 0);
+    }
+
+    void Update()
+    {
+        // set rotation
+        cameraRoot.transform.rotation = Quaternion.Euler(rotState);
+
+        // update state based on inputs
+        rotState = ApplyInput(rotState);
+    }
+
+    private Vector3 ApplyInput(Vector3 state)
+    {
+        float dHorizontal = mouseInput.GetHorizontalDrag();
+        float dVertial = mouseInput.GetVerticalDrag();
+
+        state.y += dHorizontal;
+        state.x -= dVertial;
+
+        state.x = Mathf.Clamp(state.x, -90, 90);
+
+        return state;
+    }
+}
---
+++ Unity-Proj/Assets/Scripts/3D/MouseInput.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseInput : MonoBehaviour
+{
+    private Vector3 prevMousePos;
+    private float dx;
+    private float dy;
+
+    private void Start()
+    {
+        prevMousePos = Input.mousePosition;
+    }
+
+    private void Update()
+    {
+        Vector3 current = Input.mousePosition;
+
+        dx = current.x - prevMousePos.x;
+        dy = current.y - prevMousePos.y;
+
+        prevMousePos.x = current.x;
+        prevMousePos.y = current.y;
+    }
+
+    private void OnMouseDown()
+    {
+        prevMousePos = Input.mousePosition;
+    }
+
+    public float GetVerticalDrag()
+    {
+        return Input.GetMouseButton(button: 0) ? dy : 0f;
+    }
+
+    public float GetHorizontalDrag()
+    {
+        return Input.GetMouseButton(button: 0) ? dx : 0f;
+    }
+}
