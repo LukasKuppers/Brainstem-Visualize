4e168f9ed377bfe5147a9c882b1c82227552d982
Lukas Kuppers <lkuppers11@gmail.com>
2021-01-10 12:53:37 -0800

    Add toggle slices button

---
+++ Unity-Proj/Assets/Scripts/UI/SliceDisplayManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class SliceDisplayManager : MonoBehaviour
+{
+    private static readonly string SHOW_TEXT = "Show Slices";
+    private static readonly string HIDE_TEXT = "Hide Slices";
+
+    [SerializeField]
+    private List<GameObject> slices;
+    [SerializeField]
+    private string buttonName;
+    [SerializeField]
+    private bool isHidden;
+
+    private Button toggleBtn;
+
+    private void OnEnable()
+    {
+        var root = GetComponent<UIDocument>().rootVisualElement;
+
+        toggleBtn = root.Q<Button>(name: buttonName);
+
+        SetState(isHidden);
+        RegisterToggleButton();
+    }
+
+    private void SetState(bool isHidden)
+    {
+        foreach (GameObject slice in slices)
+        {
+            slice.SetActive(!isHidden);
+        }
+
+        string btnText = isHidden ? SHOW_TEXT : HIDE_TEXT;
+        toggleBtn.text = btnText;
+    }
+
+    private void RegisterToggleButton()
+    {
+        toggleBtn.RegisterCallback<ClickEvent>(ev =>
+        {
+            isHidden = !isHidden;
+            SetState(isHidden);
+        });
+    }
+}
