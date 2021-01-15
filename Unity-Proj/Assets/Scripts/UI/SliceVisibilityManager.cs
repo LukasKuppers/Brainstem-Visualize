b0a42362f20584b405e729901a43c6d98627251c
Lukas Kuppers <lkuppers11@gmail.com>
2021-01-14 18:18:15 -0800

    Add TMP and recreate show/hide slices button with new UI system

---
+++ Unity-Proj/Assets/Scripts/UI/SliceVisibilityManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SliceVisibilityManager : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> slices;
+
+    private static readonly string TEXT_SHOW = "Show Slices";
+    private static readonly string TEXT_HIDE = "Hide Slices";
+
+    private Button button;
+    private TextMeshProUGUI btnText;
+
+    private bool isHidden = false;
+
+    private void Start()
+    {
+        button = gameObject.GetComponent<Button>();
+        btnText = gameObject.GetComponentInChildren<TextMeshProUGUI>();
+
+        button.onClick.AddListener(delegate { OnClick(); });
+        SetState(isHidden);
+    }
+
+    private void OnClick()
+    {
+        isHidden = !isHidden;
+        SetState(isHidden);
+    }
+
+    private void SetState(bool isHIdden)
+    {
+        foreach (var slice in slices)
+        {
+            slice.SetActive(!isHidden);
+        }
+
+        string text = isHidden ? TEXT_SHOW : TEXT_HIDE;
+        btnText.text = text;
+    }
+}
