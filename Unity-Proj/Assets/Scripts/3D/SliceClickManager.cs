3f6850fb00d8d3c63f8dea57579de6867286993c
Lukas Kuppers <lkuppers11@gmail.com>
2021-01-15 16:14:44 -0800

    Add slice click manager

---
+++ Unity-Proj/Assets/Scripts/3D/SliceClickManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliceClickManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject windowPrefab;
+    [SerializeField]
+    private GameObject canvas;
+
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            OnClick();
+        }
+    }
+
+    private void OnClick()
+    {
+        GameObject newWindow = Instantiate(windowPrefab);
+        newWindow.name = "My New Window!";
+        newWindow.transform.SetParent(canvas.transform);
+    }
+}
