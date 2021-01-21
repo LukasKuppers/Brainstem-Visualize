bf41ee165a3f92250a94082d7474bc01700e761a
Lukas Kuppers <lkuppers11@gmail.com>
2021-01-20 16:49:04 -0800

    Created template for 3d labels

---
+++ Unity-Proj/Assets/Scripts/3D/Label.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Label : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject canvas;
+    [SerializeField]
+    private GameObject cameraObject;
+    [SerializeField]
+    private GameObject buttonPrefab;
+    [SerializeField]
+    private string label;
+
+    private Camera cam;
+
+    private GameObject button;
+    private GameObject text;
+    private RectTransform btnRt;
+
+    private void Start()
+    {
+        cam = cameraObject.GetComponent<Camera>();
+
+        // spawn button
+        button = Instantiate(buttonPrefab);
+        button.transform.SetParent(canvas.transform);
+        btnRt = button.GetComponent<RectTransform>();
+
+        button.GetComponent<LabelButton>().SetupButton(label: label);
+    }
+
+    private void Update()
+    {
+        if(Physics.Linecast(cameraObject.transform.position, transform.position))
+        {
+            button.SetActive(false);
+        }
+        else
+        {
+            button.SetActive(true);
+            SetButtonPosition();
+        }
+    }
+
+    private void SetButtonPosition()
+    {
+        var screenPoint = cam.WorldToScreenPoint(transform.position);
+        btnRt.position = screenPoint;
+    }
+}
---
+++ Unity-Proj/Assets/Scripts/UI/LabelButton.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class LabelButton : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject labelObject;
+    [SerializeField]
+    private GameObject textObject;
+
+    private bool isHidden;
+
+    public void SetupButton(string label)
+    {
+        isHidden = true;
+
+        // button setup
+        Button button = gameObject.GetComponent<Button>();
+        button.onClick.AddListener(delegate { OnClick(); });
+
+        // label setup
+        textObject.GetComponent<TextMeshProUGUI>().text = label;
+
+        SetState(isHidden);
+    }
+
+    private void OnClick()
+    {
+        isHidden = !isHidden;
+        SetState(isHidden);
+    }
+
+    private void SetState(bool isHidden)
+    {
+        labelObject.SetActive(!isHidden);
+    }
+}
