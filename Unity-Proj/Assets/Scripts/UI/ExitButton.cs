17020847b15d7beb406e823ea37edfb9c56aa208
Lukas Kuppers <lkuppers11@gmail.com>
2021-02-09 22:34:24 -0800

    Added toggleable references area and exit button

---
+++ Unity-Proj/Assets/Scripts/UI/ExitButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExitButton : MonoBehaviour
+{
+    private Button button;
+
+    private void Start()
+    {
+        button = gameObject.GetComponent<Button>();
+
+        button.onClick.AddListener(delegate { OnClick(); });
+    }
+
+    private void OnClick()
+    {
+        Application.Quit();
+    }
+}
---
+++ Unity-Proj/Assets/Scripts/UI/ShowReferencesButton.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ShowReferencesButton : MonoBehaviour
+{
+    private static readonly string SHOW_TEXT = "Show References";
+    private static readonly string HIDE_TEXT = "Hide References";
+
+    [SerializeField]
+    private GameObject textObject;
+
+    private Button button;
+    private TextMeshProUGUI buttonText;
+
+    private bool isHidden = true;
+
+    private void Start()
+    {
+        button = gameObject.GetComponent<Button>();
+        buttonText = gameObject.GetComponentInChildren<TextMeshProUGUI>();
+
+        button.onClick.AddListener(delegate { OnClick(); });
+        SetState(isHidden);
+    }
+
+    private void OnClick()
+    {
+        isHidden = !isHidden;
+        SetState(isHidden);
+    }
+
+    private void SetState(bool isHidden)
+    {
+        textObject.SetActive(!isHidden);
+
+        string display = isHidden ? SHOW_TEXT : HIDE_TEXT;
+        buttonText.text = display;
+    }
+}
